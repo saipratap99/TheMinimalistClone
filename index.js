cb25b84df6d3c251e38b0ac793bf6bfac12ad824
saipratap99 <122005067@sastra.ac.in>
2022-04-09 16:13:45 +0530

    Added fixed back button at bottom

--- index.js
+++ index.js
@@ -13,20 +13,35 @@ let our_services = document.querySelector(".our-services");
 let our_services_item = document.querySelector(".our-services-item");
 
 let logo_white = document.querySelector(".logo-white");
+let back_arrow = document.querySelector(".back-arrow");
 
 let displayContainter = (container) => {
     let curr_active = document.querySelector(".active");
-    container.classList.toggle("active");
+
+    if(!curr_active.classList.contains("home"))
+        curr_active.classList.toggle("animate-section")
+    
     curr_active.classList.toggle("active");
+
     logo_white.style.display = "none";
+    
+    container.classList.toggle("active");
+    container.classList.toggle("animate-section");
+    back_arrow.classList.toggle("show");
 }
 
 let returnBack = ()=>{
     let curr_active = document.querySelector(".active");
+
+    curr_active.classList.toggle("animate-section");
     curr_active.classList.toggle("active");
+    
     home.classList.toggle("active");
     logo_white.style.display = "block";
+    back_arrow.classList.toggle("show");
 }
+
+back_arrow.addEventListener("click",returnBack);
 
 about_us_item.addEventListener("click", ()=>{displayContainter(about_us);});
 about_us.querySelector(".return-btn").addEventListener("click", returnBack);
